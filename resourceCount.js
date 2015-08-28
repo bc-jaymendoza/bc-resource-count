5a0b8a801b7ad2415c7919afa176de8bf6991d09
Jay Mendoza <jay.mendoza@bigcommerce.com>
2015-08-27 21:25:47 -0500

    initial commit

---
+++ resourceCount.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Migrations Resource Totals
+// @namespace    http://bigcommerce.com
+// @version      0.1
+// @description  grabs product, etc. count from /admin/ninja
+// @author       Jay Mendoza
+// @match        https://*.mybigcommerce.com/manage/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$.get("/admin/ninja", function(resp) {
+	var doc = (new DOMParser).parseFromString(resp, 'text/html');
+
+	var productCount = getProductCount(doc);
+	console.log("Products: " + productCount);
+
+	var categoryCount = getCategoryCount(doc);
+	console.log("Categories: " + categoryCount);
+
+});
+
+function getProductCount(doc) {
+	return getElementByXpath('//*[@id="content"]/dl[3]/dd[2]/code', doc);
+}
+
+function getCategoryCount(doc) {
+	return getElementByXpath('//*[@id="content"]/dl[12]/dd[2]/code', doc);
+}
+
+
+function getElementByXpath(path, doc) {
+  return document.evaluate(path, doc, null, XPathResult.STRING_TYPE, null).stringValue;
+}
